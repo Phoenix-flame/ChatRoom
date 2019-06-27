aa4dbbf8f5070f94d47ae6f8dcb7b3dc1a4cffb5
Alireza <alireza17010@gamil.com>
2019-06-27 22:19:54 +0430

    Message from server and other client is ok on HTML

--- Client/GClient/GClient.py
+++ Client/GClient/GClient.py
@@ -2,20 +2,28 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, send
 from threading import Lock
 
+import Client
+t = 0
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 thread = None
 thread_lock = Lock()
-
+tmp = True
 
 def background_thread():
     while True:
         socketio.sleep(5)
         # emit('my response', {'data': 'Connected'})
+        global tmp
+        if tmp == True:
+            tmp = False
+        else :
+            tmp = True
+            
         socketio.emit('my response',
-                      {'data': 'We are Immortals...'})
+                      {'ip': '192.168.0.1', 'content': 'We are immortals', 'fromOthers': tmp})
 
 @app.route('/')
 def index():
@@ -35,7 +43,7 @@ def test_connect():
     with thread_lock:
         if thread is None:
             thread = socketio.start_background_task(background_thread)
-    emit('my response', {'data': 'Connected'})
+    emit('my response', {'ip': '192.168.0.1', 'content': 'We are immortals'})
     print("Someone is connected")
 
 @socketio.on('disconnect', namespace='')
